b3e40a8b0fb3b4b49f859bebaefc1bc146d08df4
srirajan <sriram.rajan@gmail.com>
2013-11-13 18:12:15 +0000

    sshd config

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default["sshd"] ["port"] = 2222
+default["sshd"] ["x11_forwarding"] = 'no'
+default["sshd"] ["banner"] = '/etc/issue.net'
+default["sshd"] ["permit_root"] = 'yes'
---
+++ recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: sshd
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "/etc/ssh/sshd_config" do
+    source "sshd.erb"
+    mode "0644"
+    variables(
+    :sshd_port => node['sshd']['port'],
+    :x11_forwarding => node['sshd']['x11_forwarding'],
+    :banner => node['sshd']['banner'],
+    :permit_root => node['sshd']['permit_root']
+)
+end
+
+template "/etc/issue.net" do
+    source "issue.net.erb"
+end
+
+service "sshd" do
+    case node["platform"]
+        when "redhat", "centos", "scientific", "fedora", "amazon"
+            service_name "sshd"
+        when "debian", "ubuntu", "suse"
+            service_name "ssh"
+    end
+    supports :restart => true
+    action [ :enable, :restart ]
+end
